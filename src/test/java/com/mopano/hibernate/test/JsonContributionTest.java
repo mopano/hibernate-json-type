66d89c8764e94d4e591c98c84bcb0183ac7aa4b5
Yordan Gigov <jgigov@abv.bg>
2017-01-19 11:18:49 +0200

    Add test for binding JSON objects/arrays in queries

--- src/test/java/com/mopano/hibernate/test/JsonContributionTest.java
+++ src/test/java/com/mopano/hibernate/test/JsonContributionTest.java
@@ -5,7 +5,11 @@
  */
 package com.mopano.hibernate.test;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonStructure;
@@ -16,6 +20,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Id;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import javax.persistence.Table;
 
 import org.apache.logging.log4j.LogManager;
@@ -105,7 +110,82 @@ public class JsonContributionTest {
 		}
 	}
 
-	@Entity
+	@Test
+	@SuppressWarnings("unchecked")
+	public void testParameter() {
+		// jsonb_contains(jsonb, jsonb)
+
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		JsonProvider jp = JsonProvider.provider();
+
+		try {
+			//version check
+			Query vq = em.createNativeQuery("SELECT version()");
+			String versionString = vq.getSingleResult().toString();
+			Pattern pat = Pattern.compile("^PostgreSQL (\\d{1,2})\\.(\\d+)\\.");
+			Matcher mat = pat.matcher(versionString);
+			if (!mat.find()) {
+				// incompatible database
+				return;
+			}
+			int majorVersion = Integer.parseInt(mat.group(1));
+			int minorVersion = Integer.parseInt(mat.group(2));
+			if (majorVersion == 9 && minorVersion < 4) {
+				// this version does not support jsonb type, which is the only one which we can test
+				// binding parameters on.
+				return;
+			}
+
+			MyEntity entity = new MyEntity();
+			entity.id = 5l;
+			entity.js = jp.createArrayBuilder().add("nothing").add("else").add("matters").build();
+			entity.jo = null;
+			entity.ja = null;
+			LOGGER.info("Persisting entity: " + entity);
+			em.persist(entity);
+
+			entity = new MyEntity();
+			entity.id = 6l;
+			entity.js = jp.createArrayBuilder().add("str").add(false).add(0l).build();
+			entity.jo = jp.createObjectBuilder().add("thearray", jp.createArrayBuilder().add("str").add("unique").add(false).add(0l).build()).build();
+			entity.ja = jp.createArrayBuilder().add(4).add("word").build();
+			LOGGER.info("Persisting entity: " + entity);
+			em.persist(entity);
+			em.flush();
+			vq = null; // release pointer
+			em.getTransaction().commit();
+			// end setup-data
+
+			em.getTransaction().begin();
+			Query q = em.createNativeQuery("SELECT * FROM json_entity WHERE jsonb_contains( CAST(jo AS jsonb), :js )", MyEntity.class);
+			// Cannot bind JsonArray values, because they have all chosen to extend java.util.AbstractList
+			JsonStructure js = jp.createObjectBuilder().add("thearray", jp.createArrayBuilder().add("unique").build()).build();
+			q.setParameter("js", js);
+			List<MyEntity> results = q.getResultList();
+			assertEquals("Wrong number of entities returned", 1, results.size());
+			MyEntity fetched = results.get(0);
+			assertEquals("Wrong entity returned", 6L, fetched.id.longValue());
+
+			// this part of the test requires a proposed feature change to Hibernate 5.2
+			q = em.createNativeQuery("SELECT * FROM json_entity WHERE jsonb_contains( CAST(js AS jsonb), :js )", MyEntity.class);
+			js = jp.createArrayBuilder().add("nothing").build();
+			q.setParameter("js", js);
+			results = q.getResultList();
+			assertEquals("Wrong number of entities returned", 1, results.size());
+			fetched = results.get(0);
+			assertEquals("Wrong entity returned", 5L, fetched.id.longValue());
+		}
+		finally {
+			if (em.getTransaction() != null && em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+			em.close();
+		}
+	}
+
+	@Entity(name = "JSON_ENTITY")
 	@Table(name = "json_entity")
 	public static class MyEntity {
 
